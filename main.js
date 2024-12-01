c3f848014b822b62163e1b3dba5491c600842fc4
Trystan211 <fcpfelyn@gmail.com>
2024-12-01 16:12:07 +0800

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 
 // Dynamic Light
 const dynamicLight = new THREE.PointLight(0xffffff, 4, 50);
-dynamicLight.position.set(15, 10, 15); // Initial light position
+dynamicLight.position.set(25, 10, 25); // Initial light position
 scene.add(dynamicLight);
 
 // Ocean Geometry
