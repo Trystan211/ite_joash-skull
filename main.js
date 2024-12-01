b3007b5de65af29a1be0c10719105889455e3f8e
Trystan211 <fcpfelyn@gmail.com>
2024-12-01 12:51:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ loader.load(
     'https://trystan211.github.io/ite_joash-skull/low_poly_skull.glb', // Replace with the actual skeleton model URL
     (gltf) => {
         skeleton = gltf.scene;
-        skeleton.position.set(1, -1, 1); // Position the skeleton
+        skeleton.position.set(1, -2, 1); // Position the skeleton
         scene.add(skeleton);
 
         // Check and scale skeleton if necessary
@@ -86,7 +86,7 @@ loader.load(
         box.getSize(size);
         console.log('Skeleton dimensions:', size);
 
-        skeleton.scale.set(10, 10, 10); // Adjust scale to fit the scene
+        skeleton.scale.set(6, 6, 6); // Adjust scale to fit the scene
     },
     undefined,
     (error) => {
